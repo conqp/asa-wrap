7374c7860c5083668106a77f2955889141df154a
Richard Neumann <mail@richard-neumann.de>
2023-11-01 01:01:19 +0100

    Fix section for MaxPlayers

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 const SERVER_SETTINGS_SECTION: &str = "ServerSettings";
+const SCRIPT_ENGINE_GAME_SESSION_SECTION: &str = "/Script/Engine.GameSession";
 const ACTIVE_MODS_KEY: &str = "ActiveMods";
 pub const MAX_PLAYERS: &str = "MaxPlayers";
 pub const SERVER_PASSWORD: &str = "ServerPassword";
@@ -10,33 +11,45 @@ pub type Section = HashMap<String, Option<String>>;
 pub type IniFile = HashMap<String, Section>;
 
 pub trait GameUserSettings {
-    fn get_server_settings(&self) -> Option<Section>;
+    fn server_settings_section(&self) -> Option<Section>;
 
-    fn get_server_setting(&self, key: &str) -> Option<String> {
-        self.get_server_settings()
-            .and_then(|server_settings| server_settings.get(key).and_then(Clone::clone))
+    fn script_engine_game_session_section(&self) -> Option<Section>;
+
+    fn server_setting(&self, key: &str) -> Option<String> {
+        self.server_settings_section()
+            .and_then(|section| section.get(key).and_then(Clone::clone))
+    }
+
+    fn script_engine_game_session(&self, key: &str) -> Option<String> {
+        self.script_engine_game_session_section()
+            .and_then(|section| section.get(key).and_then(Clone::clone))
     }
 
     fn active_mods(&self) -> Option<String> {
-        self.get_server_setting(&ACTIVE_MODS_KEY.to_ascii_lowercase())
+        self.server_setting(&ACTIVE_MODS_KEY.to_ascii_lowercase())
     }
 
     fn server_password(&self) -> Option<String> {
-        self.get_server_setting(&SERVER_PASSWORD.to_ascii_lowercase())
+        self.server_setting(&SERVER_PASSWORD.to_ascii_lowercase())
     }
 
     fn server_admin_password(&self) -> Option<String> {
-        self.get_server_setting(&SERVER_ADMIN_PASSWORD.to_ascii_lowercase())
+        self.server_setting(&SERVER_ADMIN_PASSWORD.to_ascii_lowercase())
     }
 
     fn max_players(&self) -> Option<String> {
-        self.get_server_setting(&MAX_PLAYERS.to_ascii_lowercase())
+        self.server_setting(&MAX_PLAYERS.to_ascii_lowercase())
     }
 }
 
 impl GameUserSettings for IniFile {
-    fn get_server_settings(&self) -> Option<Section> {
+    fn server_settings_section(&self) -> Option<Section> {
         self.get(&SERVER_SETTINGS_SECTION.to_ascii_lowercase())
+            .cloned()
+    }
+
+    fn script_engine_game_session_section(&self) -> Option<Section> {
+        self.get(&SCRIPT_ENGINE_GAME_SESSION_SECTION.to_ascii_lowercase())
             .cloned()
     }
 }
